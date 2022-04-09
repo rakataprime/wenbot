83acdd8464e96c646478a887f405d85ad6ea77e3
rakataprime <logan@rakatatech.com>
2022-04-09 12:34:25 -0600

    add bot example without onchain integration

--- bot.py
+++ bot.py
@@ -10,8 +10,9 @@ echo_bot
 
 import simplematrixbotlib as botlib
 import pandas as pd 
+import subprocess, aiofiles
 
-creds = botlib.Creds("https://matrix.org", "@sockpuppetarmy:matrix.org", "NOTPASSWORD")
+creds = botlib.Creds("https://matrix.org", "@sockpuppetarmy:matrix.org", "YOUSHALLNOTPASS")
 bot = botlib.Bot(creds)
 PREFIX = ''
 
@@ -26,9 +27,17 @@ async def echo(room, message):
                   df = pd.read_html("https://coinmarketcap.com/currencies/juno/", )
                   juno_price = df[0].iloc[0,1]
                   await bot.api.send_text_message(room.room_id, f"juno price is {juno_price}" )
-            if match.command( ""):
-                  df = pd.read_html("https://coinmarketcap.com/currencies/juno/", )
-                  juno_price = df[0].iloc[0,1]
-                  await bot.api.send_text_message(room.room_id, f"juno price is {juno_price}" )
+            if match.command("get contract"):
+                  subprocess.run(["junod",  "query", "wasm", "code", f"{match.event.body().replace("get contract ", " ").replace(" ", "")}", "example.wasm", "--node", "https://rpc-juno.nodes.guru:443", "--chain-id" , "juno-1"])
+                  
+                  file_stat = await aiofiles.stat("example.wasm")
+                  async with aiofiles.open("example.wasm", "r+b") as f:
+                        resp, maybe_keys = await bot.api.async_client.upload(
+                              f,
+                              content_type="text/plain",
+                              filename="example.wasm",
+                              filesize=file_stat.st_size()
+                        )
+
 
 bot.run()
